b6129815119239981fb1649328e5b0bf2ecd8459
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-09-18 16:19:58 -0700

    Fixed a few things

--- assets/js/entrybuilder.js
+++ assets/js/entrybuilder.js
@@ -3,21 +3,21 @@
 function generate(){
     var devitom = document.getElementById("meters").value
 
-    document.getElementById("var1").innerHTML = (Math.round(devitom/1.47 * 100) / 100).toFixed(2) + " Dev1t0s (d0)"
+    document.getElementById("var1").innerHTML = (Math.round(devitom/1.47 * 100) / 100).toFixed(2) + " Long Dev1t0s (Ld0)"
 
 
 
     var devitof = document.getElementById("feet").value
 
-    document.getElementById("var2").innerHTML = (Math.round(devitof/4.11 * 100) / 100).toFixed(2) + " Dev1t0s (d0)"
+    document.getElementById("var2").innerHTML = (Math.round(devitof/4.11 * 100) / 100).toFixed(2) + " Long Dev1t0s (Ld0)"
     
     var devitom = document.getElementById("mass").value
 
-    document.getElementById("var3").innerHTML = (Math.round(devitom/56.7 * 100) / 100).toFixed(2) + " Dev1t0s (d0)"
+    document.getElementById("var3").innerHTML = (Math.round(devitom/56.7 * 100) / 100).toFixed(2) + " Mass Dev1t0s (Md0)"
 
     var devitoo = document.getElementById("weighto").value
 
-    document.getElementById("var4").innerHTML = (Math.round(devitoo/2 * 100) / 100).toFixed(2) + " Dev1t0s (d0)"
+    document.getElementById("var4").innerHTML = (Math.round(devitoo/2 * 100) / 100).toFixed(2) + " Mass Dev1t0s (Md0)"
     
 
 }
