8c3ac5112e1f06ae016d5e4f24d1eb44e556e04f
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-09-18 18:42:50 -0700

    added foot conversion back in

--- assets/js/entrybuilder.js
+++ assets/js/entrybuilder.js
@@ -7,19 +7,25 @@ function generate(){
 
 
 
-    var devitof = document.getElementById("inches").value
-    document.getElementById("var2").innerHTML = (Math.round(devitof/59 * 100) / 100).toFixed(2) + " Long Dev1t0s (Ld0)"
+    var devitof = document.getElementById("feet").value
+    document.getElementById("var2").innerHTML = (Math.round(devitof*12/59 * 100) / 100).toFixed(2) + " Long Dev1t0s (Ld0)"
+
+    var devitoi = document.getElementById("inches").value
+    vari = (Math.round(devitoi/59 * 100) / 100).toFixed(2)
+
+
+    document.getElementById("var3").innerHTML = (Math.round(devitoi/59 * 100) / 100).toFixed(2) + " Long Dev1t0s (Ld0)"
 
 
    /* document.getElementById("var2").innerHTML = (Math.round(devitof*1.2/4.93 * 100) / 100).toFixed(2) + " Long Dev1t0s (Ld0)" */
     
     var devitom = document.getElementById("mass").value
 
-    document.getElementById("var3").innerHTML = (Math.round(devitom/56.7 * 100) / 100).toFixed(2) + " Mass Dev1t0s (Md0)"
+    document.getElementById("var4").innerHTML = (Math.round(devitom/56.7 * 100) / 100).toFixed(2) + " Mass Dev1t0s (Md0)"
 
     var devitoo = document.getElementById("weighto").value
 
-    document.getElementById("var4").innerHTML = (Math.round(devitoo/2 * 100) / 100).toFixed(2) + " Mass Dev1t0s (Md0)"
+    document.getElementById("var5").innerHTML = (Math.round(devitoo/2 * 100) / 100).toFixed(2) + " Mass Dev1t0s (Md0)"
     
 
 }
